7d091c074d53d47a7d6aefd873727194d09da421
Stanislav <zrozhevsky@gmail.com>
2019-03-06 14:11:06 +0200

    Added UserContext - context class from DbContext for EntoryFrameworkCore.

---
+++ Sources/Projects/Users.DataAccess.Database/Contexts/UserContext.cs
@@ -0,0 +1,18 @@
+﻿namespace Users.DataAccess.Database.Contexts
+{
+    using Microsoft.EntityFrameworkCore;
+    using Users.DataAccess.DataModel.Types;
+
+    public class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<UserRole> UserRoles { get; set; }
+
+        public DbSet<Account> Accounts { get; set; }
+
+        public DbSet<AccountRole> AccountRoles { get; set; }
+
+        public DbSet<AccountAddress> AccountAddresses { get; set; }
+    }
+}
