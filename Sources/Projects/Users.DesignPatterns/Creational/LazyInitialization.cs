c547ae55279c86b4eba8c6502704fdacfb81e8a0
Stanislav <zrozhevsky@gmail.com>
2019-03-13 23:21:59 +0200

    Added Singleton and Lazy Initialization.

---
+++ Sources/Projects/Users.DesignPatterns/Creational/LazyInitialization.cs
@@ -0,0 +1,33 @@
+﻿namespace Users.DesignPatterns.Creational
+{
+    public class LazyInitialization<T> 
+        where T : new()
+    {
+        protected LazyInitialization()
+        {
+
+        }
+
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                // To eliminate the possibility of creating two objects in a multithreaded application.
+                if (_instance == null)
+                {
+                    lock (typeof(T))
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new T();
+                        }
+                    }
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
---
+++ Sources/Projects/Users.DesignPatterns/Creational/Singleton.cs
@@ -0,0 +1,56 @@
+﻿namespace Users.DesignPatterns.Creational
+{
+    using System;
+    using System.Reflection;
+
+    public class Singleton<T>
+        where T : new()
+    {
+        protected Singleton()
+        {
+
+        }
+
+        private sealed class SingletonCreator<TS>
+            where TS : new()
+        {
+            public static TS CreatorInstance { get; } = (TS) typeof(TS).GetConstructor(
+                    BindingFlags.Instance | BindingFlags.NonPublic,
+                    null,
+                    new Type[0],
+                    new ParameterModifier[0]
+                )
+                ?.Invoke(null);
+        }
+
+        public static T Instance => SingletonCreator<T>.CreatorInstance;
+    }
+
+    /*public class Singleton<T>
+        where T : new()
+    {
+        protected Singleton()
+        {
+
+        }
+
+        private sealed class SingletonCreator<TS> 
+            where TS : new()
+        {
+            public static TS Instance { get; } = new TS();
+        }
+
+        public static T Instance => SingletonCreator<T>.Instance;
+    }*/
+
+    /*public class Singleton<T> 
+        where T : new()
+    {
+        protected Singleton()
+        {
+
+        }
+
+        public static T Instance { get; } = new T();
+    }*/
+}
