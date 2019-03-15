3a0a95e3d64d5e4ef46081fce33d3c356134f5c2
Stanislav <zrozhevsky@gmail.com>
2019-03-15 09:44:27 +0200

    Added new project Custom Attributes to solution. Added custom attributes - replace strings attribute and replace chars attribute.

---
+++ Sources/Projects/Users.CustomAttributes/ReplaceCharsAttribute.cs
@@ -0,0 +1,31 @@
+﻿namespace Users.CustomAttributes
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Method)]
+    public class ReplaceCharsAttribute : Attribute
+    {
+        private char _oldValue;
+        private char _newValue;
+
+        public ReplaceCharsAttribute(
+            char oldValue,
+            char newValue)
+        {
+            _oldValue = oldValue;
+            _newValue = newValue;
+        }
+
+        public virtual char OldValue
+        {
+            get { return _oldValue; }
+            set { _newValue = value; }
+        }
+
+        public virtual char NewValue
+        {
+            get { return _newValue; }
+            set { _newValue = value; }
+        }
+    }
+}
---
+++ Sources/Projects/Users.CustomAttributes/ReplaceStringsAttribute.cs
@@ -0,0 +1,31 @@
+﻿namespace Users.CustomAttributes
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Method)]
+    public class ReplaceStringsAttribute : Attribute
+    {
+        private string _oldValue;
+        private string _newValue;
+
+        public ReplaceStringsAttribute(
+            string oldValue,
+            string newValue)
+        {
+            _oldValue = oldValue;
+            _newValue = newValue;
+        }
+
+        public virtual string OldValue
+        {
+            get { return _oldValue; }
+            set { _newValue = value; }
+        }
+
+        public virtual string NewValue
+        {
+            get { return _newValue; }
+            set { _newValue = value; }
+        }
+    }
+}
