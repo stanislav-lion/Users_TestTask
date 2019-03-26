fa1f97c63726d0f76a96ac0886e3152e0ba4fda4
Stanislav <zrozhevsky@gmail.com>
2019-03-26 13:54:33 +0200

    Added interface - context repositories async.

---
+++ Sources/Projects/Users.DataAccess.Repository/Async/IRepositoryContextAsync.cs
@@ -0,0 +1,61 @@
+﻿namespace Users.DataAccess.Repository.Async
+{
+    using System;
+
+    /// <summary>
+    ///     Repository context.
+    /// </summary>
+    public interface IRepositoryContextAsync : IDisposable
+    {
+        /// <summary>
+        ///     Gets account address repository.
+        /// </summary>
+        IAccountAddressRepositoryAsync AccountAddressRepositoryAsync { get; }
+
+        /// <summary>
+        ///     Gets account repository.
+        /// </summary>
+        IAccountRepositoryAsync AccountRepositoryAsync { get; }
+
+        /// <summary>
+        ///     Gets account role repository.
+        /// </summary>
+        IAccountRoleRepositoryAsync AccountRoleRepositoryAsync { get; }
+
+        /// <summary>
+        ///     Gets user repository.
+        /// </summary>
+        IUserRepositoryAsync UserRepositoryAsync { get; }
+
+        /// <summary>
+        ///     Gets user role repository.
+        /// </summary>
+        IUserRoleRepositoryAsync UserRoleRepositoryAsync { get; }
+
+        /// <summary>
+        ///     Gets service broker repository.
+        /// </summary>
+        //IServiceBrokerRepository ServiceBrokerRepository { get; }
+
+        /// <summary>
+        ///     Begins changes.
+        /// </summary>
+        void BeginChangesAsync();
+
+        /// <summary>
+        ///     Saves changes.
+        /// </summary>
+        void SaveChangesAsync();
+
+        /// <summary>
+        ///     Cancel changes.
+        /// </summary>
+        void CancelChangesAsync();
+
+        /// <summary>
+        ///     Performs changes.
+        /// </summary>
+        /// <param name="changes">Changes to perform.</param>
+        void PerformChangesAsync(Func<bool> changes);
+    }
+}
---
+++ Sources/Projects/Users.DataAccess.Repository/Async/IRepositoryContextFactoryAsync.cs
@@ -0,0 +1,14 @@
+﻿namespace Users.DataAccess.Repository.Async
+{
+    /// <summary>
+    ///     Repository context factory.
+    /// </summary>
+    public interface IRepositoryContextFactoryAsync
+    {
+        /// <summary>
+        ///     Creates repository context.
+        /// </summary>
+        /// <returns>Repository context.</returns>
+        IRepositoryContextAsync CreateRepositoryContextAsync();
+    }
+}
