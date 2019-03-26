9db4ddecac489a94a757f3a79c6644980608182b
Stanislav <zrozhevsky@gmail.com>
2019-03-26 13:18:24 +0200

    Added interface - account address repository async.

---
+++ Sources/Projects/Users.DataAccess.Repository/Async/IAccountAddressRepositoryAsync.cs
@@ -0,0 +1,31 @@
+﻿namespace Users.DataAccess.Repository.Async
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Users.DataAccess.DataModel.Types;
+
+    /// <summary>
+    ///     Account address repository.
+    /// </summary>
+    public interface IAccountAddressRepositoryAsync
+    {
+        /// <summary>
+        ///     Gets all account adresses.
+        /// </summary>
+        IQueryable<AccountAddress> AccountAddresses { get; }
+
+        /// <summary>
+        ///     Gets account address.
+        /// </summary>
+        /// <param name="accountId">The identifier of account.</param>
+        /// <returns>The address of account.</returns>
+        Task<AccountAddress> GetAsync(int accountId);
+
+        /// <summary>
+        ///     Adds or updates account address.
+        /// </summary>
+        /// <param name="accountAddress">The address of account.</param>
+        /// <returns>The number of state entries written to the database.</returns>
+        Task<int> UpsertAsync(AccountAddress accountAddress);
+    }
+}
