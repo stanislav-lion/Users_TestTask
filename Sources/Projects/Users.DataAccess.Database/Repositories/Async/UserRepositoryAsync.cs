1850e11f5addb7994edbd85394dc14c7fc960b19
Stanislav <zrozhevsky@gmail.com>
2019-03-31 23:35:52 +0300

    Addes new async repositories.

---
+++ Sources/Projects/Users.DataAccess.Database/Repositories/Async/UserRepositoryAsync.cs
@@ -0,0 +1,61 @@
+﻿namespace Users.DataAccess.Database.Repositories.Async
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Users.DataAccess.DataModel.Types;
+    using Users.DataAccess.Repository.Async;
+
+    public class UserRepositoryAsync : IUserRepositoryAsync
+    {
+        public Task<IQueryable<User>> UsersAsync => throw new NotImplementedException();
+
+        public Task<IQueryable<User>> UsersWithoutPasswordsAsync => throw new NotImplementedException();
+
+        public Task<int> DeleteAsync(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteByAccountAsync(int accountId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetAsync(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetAsync(Guid userGuid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetAsync(string logonName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<User>> GetByAccountAsync(int accountId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int?> GetIdAsync(Guid userGuid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int?> GetIdAsync(string logonName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpsertAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Sources/Projects/Users.DataAccess.Database/Repositories/Async/UserRoleRepositoryAsync.cs
@@ -0,0 +1,38 @@
+﻿namespace Users.DataAccess.Database.Repositories.Async
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Users.DataAccess.DataModel.Types;
+    using Users.DataAccess.Repository.Async;
+
+    public class UserRoleRepositoryAsync : IUserRoleRepositoryAsync
+    {
+        public Task<IQueryable<UserRole>> UserRolesAsync => throw new System.NotImplementedException();
+
+        public Task<int> AddAsync(UserRole userRole)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<int> DeleteAsync(int userId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<int> DeleteAsync(int userId, int accountRoleId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<UserRole> GetAsync(int userId, int accountRoleId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<UserRole>> GetAsync(int userId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
