609913b5422632f20f17f70c9783328ac5a1d041
Stanislav <zrozhevsky@gmail.com>
2019-04-01 23:50:26 +0300

    Fixed async user repository - implementation methods.

--- Sources/Projects/Users.DataAccess.Database/Repositories/Async/UserRepositoryAsync.cs
+++ Sources/Projects/Users.DataAccess.Database/Repositories/Async/UserRepositoryAsync.cs
@@ -4,58 +4,161 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
+    using Users.DataAccess.Database.BaseRepositories;
+    using Users.DataAccess.Database.Contexts;
     using Users.DataAccess.DataModel.Types;
     using Users.DataAccess.Repository.Async;
 
-    public class UserRepositoryAsync : IUserRepositoryAsync
+    /// <inheritdoc cref="IUserRepositoryAsync" />
+    public class UserRepositoryAsync : UserContextEntityRepository, IUserRepositoryAsync
     {
-        public Task<IQueryable<User>> UsersAsync => throw new NotImplementedException();
-
-        public Task<IQueryable<User>> UsersWithoutPasswordsAsync => throw new NotImplementedException();
-
-        public Task<int> DeleteAsync(int userId)
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="UserRepository" /> class.
+        /// </summary>
+        /// <param name="userContext">Database user context.</param>
+        public UserRepositoryAsync(UserContext userContext)
+            : base(userContext)
         {
-            throw new NotImplementedException();
+
         }
 
-        public Task<int> DeleteByAccountAsync(int accountId)
+        /// <inheritdoc />
+        public Task<IQueryable<User>> UsersAsync => GetUsers();
+
+        /// <inheritdoc />
+        public Task<IQueryable<User>> UsersWithoutPasswordsAsync => GetUsersWithoutPasswords();
+
+        /// <inheritdoc />
+        public async Task<User> GetAsync(int userId)
         {
-            throw new NotImplementedException();
+            return await Task.Run(
+                () =>
+                {
+                    return UserContext.User
+                        .FirstOrDefault(user => user.UserId == userId);
+                });
         }
 
-        public Task<User> GetAsync(int userId)
+        /// <inheritdoc />
+        public async Task<User> GetAsync(Guid userGuid)
         {
-            throw new NotImplementedException();
+            return await Task.Run(
+                () =>
+                {
+                    return UserContext.User
+                        .FirstOrDefault(user => user.UserGuid == userGuid);
+                });
         }
 
-        public Task<User> GetAsync(Guid userGuid)
+        /// <inheritdoc />
+        public async Task<User> GetAsync(string logonName)
         {
-            throw new NotImplementedException();
+            return await Task.Run(
+                () =>
+                {
+                    return UserContext.User
+                        .FirstOrDefault(user => user.LogonName.Trim().Equals(logonName));
+                });
         }
 
-        public Task<User> GetAsync(string logonName)
+        /// <inheritdoc />
+        public async Task<List<User>> GetByAccountAsync(int accountId)
         {
-            throw new NotImplementedException();
+            return await Task.Run(
+                () =>
+                {
+                    return UserContext.User
+                        .Where(user => user.AccountId == accountId)
+                        .ToList();
+                });
         }
 
-        public Task<List<User>> GetByAccountAsync(int accountId)
+        /// <inheritdoc />
+        public async Task<int?> GetIdAsync(Guid userGuid)
         {
-            throw new NotImplementedException();
+            User user = await GetAsync(userGuid);
+
+            if (user == null)
+            {
+                return null;
+            }
+
+            return user.UserId;
         }
 
-        public Task<int?> GetIdAsync(Guid userGuid)
+        /// <inheritdoc />
+        public async Task<int?> GetIdAsync(string logonName)
         {
-            throw new NotImplementedException();
+            User user = await GetAsync(logonName);
+
+            if (user == null)
+            {
+                return null;
+            }
+
+            return user.UserId;
         }
 
-        public Task<int?> GetIdAsync(string logonName)
+        /// <inheritdoc />
+        public async Task<int> UpsertAsync(User user)
         {
-            throw new NotImplementedException();
+            if (GetAsync(user.UserId) == null)
+            {
+                UserContext.User
+                    .Add(user);
+            }
+            else
+            {
+                UserContext.User
+                    .Update(user);
+            }
+
+            return await UserContext.SaveChangesAsync();
         }
 
-        public Task<int> UpsertAsync(User user)
+        /// <inheritdoc />
+        public async Task<int> DeleteAsync(int userId)
         {
-            throw new NotImplementedException();
+            UserContext.User
+                .Remove(await GetAsync(userId));
+
+            return await UserContext.SaveChangesAsync();
+        }
+
+        /// <inheritdoc />
+        public async Task<int> DeleteByAccountAsync(int accountId)
+        {
+            UserContext.User
+                .RemoveRange(await GetByAccountAsync(accountId));
+
+            return await UserContext.SaveChangesAsync();
+        }
+
+        private async Task<IQueryable<User>> GetUsers()
+        {
+            return await Task.Run(
+                () =>
+                {
+                    return UserContext.User;
+                });
+        }
+
+        private async Task<IQueryable<User>> GetUsersWithoutPasswords()
+        {
+            return await Task.Run(
+                () =>
+                {
+                    return UserContext.User
+                        .ToList()
+                        .Select(user =>
+                        {
+                            user.PasswordSalt = null;
+                            user.PasswordHash = null;
+
+                            return user;
+                        })
+                        .AsQueryable();
+                });
         }
     }
 }
