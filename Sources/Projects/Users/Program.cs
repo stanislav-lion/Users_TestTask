8c6c3943845bf89841c50e1c6d19aae9472b03ee
Stanislav <zrozhevsky@gmail.com>
2019-04-02 23:30:46 +0300

    Fixed Program - small fix.

--- Sources/Projects/Users/Program.cs
+++ Sources/Projects/Users/Program.cs
@@ -5,11 +5,15 @@
 
     public class Program
     {
-        public static void Main(string[] args) => 
+        public static void Main(string[] args)
+        {
             CreateWebHostBuilder(args).Build().Run();
+        }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+        }
     }
 }
