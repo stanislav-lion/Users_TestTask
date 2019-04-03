ba4004c326204a9279e461ddbcfac6e744c0760c
Stanislav <zrozhevsky@gmail.com>
2019-04-03 16:01:43 +0300

    Fixed account role repository async - implementation methods.

--- Sources/Projects/Users.DataAccess.Database/Repositories/Async/AccountRoleRepositoryAsync.cs
+++ Sources/Projects/Users.DataAccess.Database/Repositories/Async/AccountRoleRepositoryAsync.cs
@@ -3,51 +3,157 @@
     using System;
     using System.Linq;
     using System.Threading.Tasks;
+    using Users.DataAccess.Database.BaseRepositories;
+    using Users.DataAccess.Database.Contexts;
     using Users.DataAccess.DataModel.Types;
     using Users.DataAccess.Repository.Async;
 
-    public class AccountRoleRepositoryAsync : IAccountRoleRepositoryAsync
+    /// <inheritdoc cref="IAccountRoleRepositoryAsync" />
+    public class AccountRoleRepositoryAsync : UserContextEntityRepository, IAccountRoleRepositoryAsync
     {
-        public Task<IQueryable<AccountRole>> AccountRolesAsync => throw new NotImplementedException();
-
-        public Task<int> DeleteAsync(int accountRoleId)
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="AccountRoleRepositoryAsync" /> class.
+        /// </summary>
+        /// <param name="userContext">Database user context.</param>
+        public AccountRoleRepositoryAsync(UserContext userContext)
+            : base(userContext)
         {
+
+        }
+
+        /// <inheritdoc />
+        public Task<IQueryable<AccountRole>> AccountRolesAsync => GetAccountRolesAsync();
+
+        /// <inheritdoc />
+        public async Task<AccountRole> GetAsync(int accountRoleId)
+        {
+            return await Task.Run(
+                () =>
+                {
+                    return UserContext.AccountRole
+                        .FirstOrDefault(accountRole => accountRole.AccountRoleId == accountRoleId);
+                });
+        }
+
+        /// <inheritdoc />
+        public async Task<AccountRole> GetAsync(Guid accountRoleGuid)
+        {
+            return await Task.Run(
+                () =>
+                {
+                    return UserContext.AccountRole
+                        .FirstOrDefault(accountRole => accountRole.AccountRoleGuid == accountRoleGuid);
+                });
+        }
+
+        /// <inheritdoc />
+        public async Task<AccountRole> GetByAccountAsync(int accountId)
+        {
+            return await Task.Run(
+                () =>
+                {
+                    return UserContext.AccountRole
+                        .FirstOrDefault(accountRole => accountRole.AccountId == accountId);
+                });
+        }
+
+        /// <inheritdoc />
+        public async Task<int?> GetIdAsync(Guid accountRoleGuid)
+        {
+            //return await Task.Run(
+            //    () =>
+            //    {
+            //        AccountRole accountRole = GetAsync(accountRoleGuid);
+
+            //        if (accountRole == null)
+            //        {
+            //            return null;
+            //        }
+
+            //        return accountRole.AccountRoleId;
+            //    });
+
             throw new NotImplementedException();
         }
 
-        public Task<int> DeleteByAccountAsync(int accountId)
+        /// <inheritdoc />
+        public async Task<int?> GetIdAsync(
+            int accountId,
+            string roleName)
         {
+            //return await Task.Run(
+            //    () =>
+            //    {
+            //        AccountRole accountRole = GetByAccountAsync(accountId, roleName);
+
+            //        if (accountRole == null)
+            //        {
+            //            return null;
+            //        }
+
+            //        return accountRole.AccountRoleId;
+            //    });
+
             throw new NotImplementedException();
         }
 
-        public Task<AccountRole> GetAsync(int accountRoleId)
+        /// <inheritdoc />
+        public async Task<int> UpsertAsync(AccountRole accountRole)
         {
-            throw new NotImplementedException();
+            if (GetAsync(accountRole.AccountRoleId) == null)
+            {
+                UserContext.AccountRole
+                    .Add(accountRole);
+            }
+            else
+            {
+                UserContext.AccountRole
+                    .Update(accountRole);
+            }
+
+            return await UserContext.SaveChangesAsync();
         }
 
-        public Task<AccountRole> GetAsync(Guid accountRoleGuid)
+        /// <inheritdoc />
+        public async Task<int> DeleteAsync(int accountRoleId)
         {
-            throw new NotImplementedException();
+            UserContext.AccountRole
+                .Remove(await GetAsync(accountRoleId));
+
+            return await UserContext.SaveChangesAsync();
         }
 
-        public Task<AccountRole> GetByAccountAsync(int accountId)
+        /// <inheritdoc />
+        public async Task<int> DeleteByAccountAsync(int accountId)
         {
-            throw new NotImplementedException();
+            UserContext.AccountRole
+                .Remove(await GetByAccountAsync(accountId));
+
+            return await UserContext.SaveChangesAsync();
         }
 
-        public Task<int?> GetIdAsync(Guid accountRoleGuid)
+        private async Task<IQueryable<AccountRole>> GetAccountRolesAsync()
         {
-            throw new NotImplementedException();
+            return await Task.Run(
+                () =>
+                {
+                    return UserContext.AccountRole;
+                });
         }
 
-        public Task<int?> GetIdAsync(int accountId, string roleName)
+        private async Task<AccountRole> GetByAccountAsync(
+            int accountId,
+            string roleName)
         {
-            throw new NotImplementedException();
-        }
-
-        public Task<int> UpsertAsync(AccountRole accountRole)
-        {
-            throw new NotImplementedException();
+            return await Task.Run(
+                () =>
+                {
+                    return UserContext.AccountRole
+                        .FirstOrDefault(accountRole =>
+                            accountRole.AccountId == accountId &&
+                            accountRole.RoleName.Equals(roleName));
+                });
         }
     }
+}
 }
