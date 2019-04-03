73a206b504f9e5d1f48a18a2956bc74253d22059
Stanislav <zrozhevsky@gmail.com>
2019-04-03 15:45:03 +0300

    Fixed account address repository async - implementation methods.

--- Sources/Projects/Users.DataAccess.Database/Repositories/Async/AccountAddressRepositoryAsync.cs
+++ Sources/Projects/Users.DataAccess.Database/Repositories/Async/AccountAddressRepositoryAsync.cs
@@ -2,21 +2,62 @@
 {
     using System.Linq;
     using System.Threading.Tasks;
+    using Users.DataAccess.Database.BaseRepositories;
+    using Users.DataAccess.Database.Contexts;
     using Users.DataAccess.DataModel.Types;
     using Users.DataAccess.Repository.Async;
 
-    public class AccountAddressRepositoryAsync : IAccountAddressRepositoryAsync
+    /// <inheritdoc cref="IAccountAddressRepositoryAsync" />
+    public class AccountAddressRepositoryAsync : UserContextEntityRepository, IAccountAddressRepositoryAsync
     {
-        public Task<IQueryable<AccountAddress>> AccountAddressesAsync => throw new System.NotImplementedException();
-
-        public Task<AccountAddress> GetAsync(int accountId)
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="AccountAddressRepositoryAsync" /> class.
+        /// </summary>
+        /// <param name="userContext">Database user context.</param>
+        public AccountAddressRepositoryAsync(UserContext userContext)
+            : base(userContext)
         {
-            throw new System.NotImplementedException();
+
         }
 
-        public Task<int> UpsertAsync(AccountAddress accountAddress)
+        /// <inheritdoc />
+        public Task<IQueryable<AccountAddress>> AccountAddressesAsync => GetAccountAddressesAsync();
+
+        /// <inheritdoc />
+        public async Task<AccountAddress> GetAsync(int accountId)
         {
-            throw new System.NotImplementedException();
+            return await Task.Run(
+                () =>
+                {
+                    return UserContext.AccountAddress
+                        .FirstOrDefault(accountAddress => accountAddress.AccountId == accountId);
+                });
+        }
+
+        /// <inheritdoc />
+        public async Task<int> UpsertAsync(AccountAddress accountAddress)
+        {
+            if (GetAsync(accountAddress.AccountId) == null)
+            {
+                UserContext.AccountAddress
+                    .Add(accountAddress);
+            }
+            else
+            {
+                UserContext.AccountAddress
+                    .Update(accountAddress);
+            }
+
+            return await UserContext.SaveChangesAsync();
+        }
+
+        private async Task<IQueryable<AccountAddress>> GetAccountAddressesAsync()
+        {
+            return await Task.Run(
+                () =>
+                {
+                    return UserContext.AccountAddress;
+                });
         }
     }
 }
