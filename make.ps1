dd5be85db4f5a89de104900cd2930b1b3e2e1af9
Stanislav <zrozhevsky@gmail.com>
2019-04-05 23:56:09 +0300

    Fixed ps-script make.

--- make.ps1
+++ make.ps1
@@ -8,5 +8,9 @@ $scriptPath = Split-Path $MyInvocation.MyCommand.Path
 
 $startDate = Get-Date
 
+& "$scriptPath\cleanup.ps1"
+
+& "$scriptPath\scripts\build.src.ps1"
+
 $elapsed = (Get-Date) - $startDate
 Write-Output "Build duration: $elapsed"
