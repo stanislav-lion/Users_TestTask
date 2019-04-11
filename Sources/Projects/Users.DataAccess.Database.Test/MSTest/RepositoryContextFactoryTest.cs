53ad7e5ae251807775a2399435c095b03e7f4a26
Stanislav <zrozhevsky@gmail.com>
2019-04-11 16:38:08 +0300

    Fixed repository context factory test for MS-tests.

--- Sources/Projects/Users.DataAccess.Database.Test/MSTest/RepositoryContextFactoryTest.cs
+++ Sources/Projects/Users.DataAccess.Database.Test/MSTest/RepositoryContextFactoryTest.cs
@@ -1,6 +1,7 @@
 ﻿namespace Users.DataAccess.Database.Test.MSTest
 {
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Users.DataAccess.Database.RepositoryContexts;
 
     /// <summary>
     ///     Tests for the RepositoryContextFactory class.
@@ -8,6 +9,50 @@
     [TestClass]
     public class RepositoryContextFactoryTest
     {
+        /// <summary>
+        ///     OnLongRunningStoredProcedure_EventIsNotRaisedIfStoredProcedureRunsForAShortTime.
+        /// </summary>
+        [TestMethod]
+        public void OnLongRunningStoredProcedure_EventIsNotRaisedIfStoredProcedureRunsForAShortTime()
+        {
+            string actualStoredProcedureName = null;
+            int actualExecutionTimeSeconds = -1;
 
+            RepositoryContextFactory repositoryContextFactory = RepositoryContextFactory.Instance;
+
+            repositoryContextFactory.OnLongRunningStoredProcedure += (s, e) =>
+            {
+                actualStoredProcedureName = e.StoredProcedureName;
+                actualExecutionTimeSeconds = e.ExecutionTimeSeconds;
+            };
+
+            Assert.IsNull(actualStoredProcedureName);
+            Assert.AreEqual(-1, actualExecutionTimeSeconds);
+        }
+
+        /// <summary>
+        ///     OnLongRunningStoredProcedure_EventIsRaisedIfStoredProcedureRunsForALongTime.
+        /// </summary>
+        [TestMethod]
+        public void OnLongRunningStoredProcedure_EventIsRaisedIfStoredProcedureRunsForALongTime()
+        {
+            const string expectedStoredProcedureName = "LongRunningStoredProcedure";
+            int expectedExecutionTimeSeconds = /*Settings.Default.LongRunningStoredProcedureTimeSeconds*/ 5 + 1;
+            string actualStoredProcedureName = null;
+            int actualExecutionTimeSeconds = -1;
+
+            RepositoryContextFactory repositoryContextFactory = RepositoryContextFactory.Instance;
+
+            repositoryContextFactory.OnLongRunningStoredProcedure += (s, e) =>
+            {
+                actualStoredProcedureName = e.StoredProcedureName;
+                actualExecutionTimeSeconds = e.ExecutionTimeSeconds;
+            };
+
+            Assert.IsNotNull(actualStoredProcedureName);
+            Assert.AreEqual(expectedStoredProcedureName, actualStoredProcedureName);
+            Assert.AreNotEqual(-1, actualExecutionTimeSeconds);
+            Assert.IsTrue(actualExecutionTimeSeconds >= expectedExecutionTimeSeconds);
+        }
     }
 }
