7e36d3f6acf647597a279dde633cfd5ac3b15be9
Stanislav <zrozhevsky@gmail.com>
2019-04-12 23:50:22 +0300

    Added ps-script.

---
+++ Scripts/copy.test.runner.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$scriptPath = Split-Path $MyInvocation.MyCommand.Path
+
+$packagesDirectoryPath = Join-Path $scriptPath '..\Packages'
+$xUnitDirectoryPath = Get-ChildItem -Directory -Path $packagesDirectoryPath -include 'xunit.runner.console*' -Recurse -Force
+$xUnitRunnerConsoleDirectoryPath = Join-Path $xUnitDirectoryPath 'tools\net452'
+
+$testRunnerDirectoryPath = Join-Path $scriptPath "..\TestRunner"
+if (-not (Test-Path $testRunnerDirectoryPath -PathType Container))
+{
+    New-Item $testRunnerDirectoryPath -ItemType Directory
+}
+
+Copy-Item "$xUnitRunnerConsoleDirectoryPath\*" -Destination $testRunnerDirectoryPath -Recurse -Force
